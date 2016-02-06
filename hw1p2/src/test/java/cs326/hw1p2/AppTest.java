385eb7e1450dd1518e3ab7b6a17032384fac5b22
tralber2 <tralber2@ncsu.edu>
2016-02-05 21:40:22 -0500

    test

--- hw1p2/src/test/java/cs326/hw1p2/AppTest.java
+++ hw1p2/src/test/java/cs326/hw1p2/AppTest.java
@@ -2,6 +2,7 @@ package cs326.hw1p2;
 
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import twitter4j.MediaEntity;
 import twitter4j.Status;
 
 import static org.junit.Assert.*;
@@ -14,6 +15,10 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import cs326.hw1p2.Analysis.BestTweets;
+import cs326.hw1p2.Analysis.WorstTweets;
+import cs326.hw1p2.Analysis.MostRetweetedPhoto;
+import cs326.hw1p2.Analysis.MostTweetedPhoto;
+import cs326.hw1p2.Analysis.BestWord;
 /**
  * Unit test for simple App.
  */
@@ -28,11 +33,14 @@ public class AppTest
     	Status testStatus;
     	
     	testStatus = Mockito.mock(Status.class);
+    	
     	when(testStatus.getId()).thenReturn(900000L);
     	when(testStatus.getRetweetCount()).thenReturn(33);
+    	
     	statuses.add(testStatus);
     	
     	testStatus = Mockito.mock(Status.class);
+    	
     	when(testStatus.getId()).thenReturn(100000L);
     	when(testStatus.getRetweetCount()).thenReturn(3);
     	statuses.add(testStatus);
@@ -40,5 +48,178 @@ public class AppTest
     	Status bestTweet = best.mostRetweeted(statuses);
     	assertEquals(bestTweet.getId(), 900000L );
        	assertEquals(bestTweet.getRetweetCount(), 33 );
+       	
+       	
+    }
+    @Test
+    public void WorstTweets() {
+    	WorstTweets worst = new WorstTweets();
+    	List<Status> statuses = new ArrayList<Status>();
+    	Status testStatus;
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	
+    	when(testStatus.getId()).thenReturn(100000L);
+    	when(testStatus.getRetweetCount()).thenReturn(3);
+    	
+    	statuses.add(testStatus);
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	
+    	when(testStatus.getId()).thenReturn(900000L);
+    	when(testStatus.getRetweetCount()).thenReturn(33);
+    	statuses.add(testStatus);
+    	
+    	Status worstTweet = worst.leastRetweeted(statuses);
+    	assertEquals(worstTweet.getId(), 100000L );
+       	assertEquals(worstTweet.getRetweetCount(), 3 );  	
+    }
+    
+    @Test
+    public void MostRetweetedPhotoPhoto() {
+    	MostRetweetedPhoto photo = new MostRetweetedPhoto();
+    	
+    	List<Status> statuses = new ArrayList<Status>();
+    	Status testStatus;
+    	MediaEntity firstMediaEntities[] = new MediaEntity[3];
+    	MediaEntity secondMediaEntities[] = new MediaEntity[3];
+    	MediaEntity photoEntity;
+    	MediaEntity videoEntity;
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	photoEntity = Mockito.mock(MediaEntity.class);
+    	videoEntity = Mockito.mock(MediaEntity.class);
+    	
+    	when(photoEntity.getType()).thenReturn("photo");
+    	when(photoEntity.getMediaURL()).thenReturn("fail");
+    	when(videoEntity.getType()).thenReturn("video");
+    	when(videoEntity.getMediaURL()).thenReturn("fail");
+    	
+    	firstMediaEntities[0] = photoEntity;
+    	firstMediaEntities[1] = videoEntity;
+    	firstMediaEntities[2] = photoEntity;
+    	
+    	secondMediaEntities[0] = videoEntity;
+    	secondMediaEntities[1] = videoEntity;
+    	secondMediaEntities[2] = photoEntity;
+    	
+    	when(testStatus.getMediaEntities()).thenReturn(firstMediaEntities);
+    	when(testStatus.getRetweetCount()).thenReturn(3);
+    	
+    	
+    	statuses.add(testStatus);
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	photoEntity = Mockito.mock(MediaEntity.class);
+    	videoEntity = Mockito.mock(MediaEntity.class);
+    	
+    	when(photoEntity.getType()).thenReturn("photo");
+    	when(photoEntity.getMediaURL()).thenReturn("pass");
+    	when(videoEntity.getType()).thenReturn("video");
+    	when(videoEntity.getMediaURL()).thenReturn("fail");
+    	
+    	secondMediaEntities[0] = videoEntity;
+    	secondMediaEntities[1] = videoEntity;
+    	secondMediaEntities[2] = photoEntity;
+    	
+    	when(testStatus.getMediaEntities()).thenReturn(secondMediaEntities);
+    	when(testStatus.getRetweetCount()).thenReturn(33);
+    	statuses.add(testStatus);
+    	
+       	
+       	String bestPhotoURL = photo.mostRetweetedPhoto(statuses);
+       	assertTrue(bestPhotoURL.equals("pass"));
+    }
+    @Test
+    public void MostTweetedPhotoPhoto() {
+    	MostTweetedPhoto photo = new MostTweetedPhoto();
+    	
+    	List<Status> statuses = new ArrayList<Status>();
+    	Status testStatus;
+    	MediaEntity firstMediaEntities[] = new MediaEntity[3];
+    	MediaEntity secondMediaEntities[] = new MediaEntity[3];
+    	MediaEntity thirdMediaEntities[] = new MediaEntity[3];
+    	MediaEntity goodPhoto;
+    	MediaEntity badPhoto;
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	goodPhoto = Mockito.mock(MediaEntity.class);
+    	badPhoto = Mockito.mock(MediaEntity.class);
+    	
+    	when(goodPhoto.getType()).thenReturn("photo");
+    	when(goodPhoto.getMediaURL()).thenReturn("pass");
+    	when(badPhoto.getType()).thenReturn("photo");
+    	when(badPhoto.getMediaURL()).thenReturn("fail");
+    	
+    	firstMediaEntities[0] = goodPhoto;
+    	firstMediaEntities[1] = badPhoto;
+    	firstMediaEntities[2] = goodPhoto;
+    	
+    	when(testStatus.getMediaEntities()).thenReturn(firstMediaEntities);
+    	when(testStatus.getRetweetCount()).thenReturn(3);
+    	
+    	
+    	statuses.add(testStatus);
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	goodPhoto = Mockito.mock(MediaEntity.class);
+    	badPhoto = Mockito.mock(MediaEntity.class);
+    	
+    	when(goodPhoto.getType()).thenReturn("photo");
+    	when(goodPhoto.getMediaURL()).thenReturn("pass");
+    	when(badPhoto.getType()).thenReturn("photo");
+    	when(badPhoto.getMediaURL()).thenReturn("fail");
+    	
+    	secondMediaEntities[0] = badPhoto;
+    	secondMediaEntities[1] = goodPhoto;
+    	secondMediaEntities[2] = goodPhoto;
+    	
+    	when(testStatus.getMediaEntities()).thenReturn(secondMediaEntities);
+    	when(testStatus.getRetweetCount()).thenReturn(33);
+    	
+    	statuses.add(testStatus);
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	goodPhoto = Mockito.mock(MediaEntity.class);
+    	badPhoto = Mockito.mock(MediaEntity.class);
+    	
+    	when(goodPhoto.getType()).thenReturn("photo");
+    	when(goodPhoto.getMediaURL()).thenReturn("pass");
+    	when(badPhoto.getType()).thenReturn("photo");
+    	when(badPhoto.getMediaURL()).thenReturn("fail");
+    	
+    	thirdMediaEntities[0] = badPhoto;
+    	thirdMediaEntities[1] = badPhoto;
+    	thirdMediaEntities[2] = goodPhoto;
+    	
+    	when(testStatus.getMediaEntities()).thenReturn(thirdMediaEntities);
+    	when(testStatus.getRetweetCount()).thenReturn(13);
+    	
+    	statuses.add(testStatus);
+    	
+       	
+       	String bestPhotoURL = photo.mostTweetedPhoto(statuses);
+       	assertTrue(bestPhotoURL.equals("pass"));
+    }
+    @Test
+    public void BestWord() {
+    	BestWord best = new BestWord();
+
+    	List<Status> statuses = new ArrayList<Status>();
+    	Status testStatus;
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	
+    	when(testStatus.getText()).thenReturn("a a b");
+    	
+    	statuses.add(testStatus);
+    	
+    	testStatus = Mockito.mock(Status.class);
+    	
+    	when(testStatus.getText()).thenReturn("c a b");
+    	
+    	statuses.add(testStatus);
+    	
+    	assertEquals(best.mostCommonWord( statuses ), 3 );
     }
 }
--- hw1p2/src/test/java/cs326/hw1p2/WebTest.java
+++ hw1p2/src/test/java/cs326/hw1p2/WebTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -15,16 +17,22 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
 public class WebTest
 {
-	private WebDriver driver;
+	private static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() throws Exception 
+    {
+        //driver = new HtmlUnitDriver(true);
+        //System.setProperty("webdriver.chrome.driver", "/Users/gameweld/classes/326/HW1.P2/hw1p2/chromedriver");
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+    }
 	
-	@Before
-	public void setUp() throws Exception 
-	{
-		driver = new HtmlUnitDriver();
-	}
-	
+    
 	@Test
 	public void googleExists() throws Exception
 	{
@@ -51,11 +59,43 @@ public class WebTest
 		{
 			if( link.getAttribute("data-href").equals("http://agile.csc.ncsu.edu/iTrust/wiki/"))
 			{
-				break;
-			}
+				break;  
+			}  
+			
 			rank++;
 		}
 		
 		assertEquals(0, rank);
 	}
+	@Test
+	public void participationCount() {
+		this.driver.get("http://checkbox.io/studies.html");
+		WebElement search = this.driver.findElement(By.xpath("//span[@class='backers']))
+	}
+	
+	@Test
+	public void totalClosedStudies() {
+		
+	}
+	
+	@Test
+	public void canParticipateInOpenStudy() {
+		
+	}
+	
+	@Test
+	public void canEnterTextInStudy() {
+//		try {
+//		this.driver.get("http://checkbox.io/studies/?id=569e667f12101f8a12000001");
+//		WebElement search = this.driver.findElement(By.name("q"));
+//		search.sendKeys("ncsu iTrust");
+//		}catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+	}
+	@AfterClass
+    public static void  tearDown() throws Exception
+    {
+        driver.close();
+    }
 }
